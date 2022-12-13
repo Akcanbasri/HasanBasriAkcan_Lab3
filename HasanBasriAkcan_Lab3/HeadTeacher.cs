5aca55459f9b569aa86bbecdad31df953cbd01fe
Akcanbasri <akcanhasanbasri@gmail.com>
2022-12-13 18:58:11 +0300

    Add project files.

---
+++ HasanBasriAkcan_Lab3/HeadTeacher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HasanBasriAkcan_Lab3
+{
+    public class HeadTeacher : Person
+    {
+        public string TitleofPerson { get; set; }
+        public int WorkingYear { get; set; }
+        public string Department { get; set; }
+
+        public HeadTeacher(string firstName, string lastName,string titleofPerson, int workingYear, string department) : base(firstName, lastName)
+        {
+            TitleofPerson = titleofPerson;
+            WorkingYear = workingYear;  
+            Department = department;
+        }
+
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine("Title Of Person: "  + TitleofPerson + "\nWorking Year: " + WorkingYear + "\nDepartment: " + Department);
+        }
+
+    }
+}
---
+++ HasanBasriAkcan_Lab3/Program.cs
@@ -0,0 +1,7 @@
+﻿using HasanBasriAkcan_Lab3;
+
+HeadTeacher teacher1 = new HeadTeacher("Hasan Basri", "Akcan", "Head Teacher", 5, "Computer Engineering");
+Boss boss1 = new Boss("Ayşegül", "Yurdakal", 500000, 200);
+teacher1.ShowInfo();
+Console.WriteLine("------------------------------");
+boss1.ShowInfo();
